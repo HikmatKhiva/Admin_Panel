0770ee14bcb12d435a94aa25148afabf4216ec97
HikmatKhiva <94689644+HikmatKhiva@users.noreply.github.com>
2022-04-14 13:37:27 +0500

    working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import TopBar from "./components/TopBar/TopBar";
 import Home from "./Pages/Home/Home";
 import UserList from './Pages/UserList/UserList'
+import User from "./Pages/User/User";
 import './style.css';
 import { Routes,  Route} from 'react-router-dom';
 
@@ -15,6 +16,7 @@ function App() {
         <Routes>
          <Route path="/" element={<Home />} /> 
          <Route path="/users" element={<UserList />} /> 
+         <Route path="/users/:userId" element={<User />} /> 
         </Routes>
       </div>
     </div>
--- src/Pages/UserList/UserList.jsx
+++ src/Pages/UserList/UserList.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './UserList.css'
+import { DataGrid } from '@mui/x-data-grid/';
+import { userRows } from '../../Data';
+import { MdDeleteOutline } from 'react-icons/md';
+import { Link } from 'react-router-dom'
 const UserList = () => {
+  const [data, setData] = useState(userRows);
+  const handleDelete = (id) => {
+    setData(data.filter(item => item.id !== id))
+  }
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 80 },
+    {
+      field: 'username', headerName: 'User', width: 200, renderCell: (params) => {
+        return (
+          <div className='userListUser'>
+            <img src={params.row.avatar} className="userListImg" alt={params.row.username} />
+            {params.row.username}
+          </div>
+        )
+      }
+    },
+    { field: 'email', headerName: 'Email', width: 150 },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 100,
+    },
+    {
+      field: 'transaction',
+      headerName: 'Transaction',
+      width: 150
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      with: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={'/users/' + params.row.id}>
+              <button className='userListEdit'>Edit</button>
+            </Link>
+            <MdDeleteOutline className='userListDelete' onClick={() => { handleDelete(params.row.id) }} />
+          </>
+        )
+      }
+    }
+  ];
   return (
     <div className='userList'>
-        
+      <DataGrid
+        rows={data}
+        columns={columns}
+        disableSelectionOnClick
+        pageSize={8}
+        checkboxSelection
+      />
     </div>
   )
 }
